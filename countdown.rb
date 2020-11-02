31e1163be083f5ca0749215510ca3fffce084e50
Andrew Eames <ajerxs@gmail.com>
2020-11-02 01:12:01 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,10 +8,10 @@ def countdown(number)
 end
 
 def countdown_with_sleep(number)
-  sleep(1)
   until number == 0
     puts "#{number} SECOND(S)!"
     number -= 1
+    sleep(1)
   end
   "HAPPY NEW YEAR!"
 end
